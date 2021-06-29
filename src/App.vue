1a7b03d47d07b792006fb84e5466b4c1a91cee39
ZoftTi <poleorder@icloud.com>
2021-06-29 11:31:45 +0800

    qiao

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,8 @@
 
   <Menu />
 
+  <ButtonBox />
+
 </template>
 
 <script setup>
@@ -15,6 +17,7 @@ import Header from "./components/Header.vue"
 import Search from "./components/Search.vue"
 import Stars from "./components/Stars.vue"
 import Menu from "./components/common/Right_Menu.vue"
+import ButtonBox from './components/common/ButtonBox.vue';
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
